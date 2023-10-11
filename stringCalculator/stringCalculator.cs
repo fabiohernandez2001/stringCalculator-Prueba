2b79930efca84797e1d19b2cfe804a934c02bc1d
fabiohernandez2001 <fabiohmaru@gmai.com>
2023-10-11 12:33:32 +0100

    Corrección sobre la variable

--- stringCalculator/stringCalculator.cs
+++ stringCalculator/stringCalculator.cs
@@ -5,7 +5,7 @@
         public static int add(string op)
         {
             if (op.Equals("")){return 0;}
-            string[3] argvs = op.Split(',');
+            string[] argvs = op.Split(',');
             int sum = 0;
             for (int i = 0;i<3;i++)
             {
