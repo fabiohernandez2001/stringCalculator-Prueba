c3f99016ac95eeb98d6553a76b4cfd2e1fdd1845
fabiohernandez2001 <fabiohmaru@gmai.com>
2023-10-11 11:59:47 +0100

    La función add completa la suma con string como parametro

--- stringCalculator/stringCalculator.cs
+++ stringCalculator/stringCalculator.cs
@@ -1,7 +1,16 @@
 ﻿namespace stringCalculator
 {
-    public static class Stringcalculator
+    public static class stringCalculator
     {
-
+        public static int add(string op)
+        {
+            string[] argvs = op.Split(',');
+            int sum = 0;
+            for (int i = 0;i<argvs.Length;i++)
+            {
+                sum += Int32.Parse(argvs[i]);
+            }
+            return sum; 
+        }
     }
 }
--- stringCalculatorTest/stringCalculatorTest1.cs
+++ stringCalculatorTest/stringCalculatorTest1.cs
@@ -1,11 +1,23 @@
+using stringCalculator;
 namespace stringCalculatorTest
 {
     [TestClass]
-    public class UnitTest1
+    public class stringcalculatortest1
     {
+        /*Test 1: la función add tiene que retornar un resultado correcto
+         * de la suma que se le pase como parametro de tipo string
+         * 
+         * Tendrá que tener en cuenta
+         */
         [TestMethod]
-        public void TestMethod1()
+        public void addShouldSum()
         {
+            string op = "1,2";
+            int result = stringCalculator.stringCalculator.add(op); 
+            Assert.AreEqual(3, result,
+                       string.Format("Expected for '{0}'; Actual: {1}",
+                                     3, result));
         }
+
     }
 }
